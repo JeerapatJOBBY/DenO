b972ad94d2e3eb77faad757467e77a79a44ad39d
job.ljee555@gmail.com <job.ljee555@gmail.com>
2020-06-12 23:30:11 +0700

    button

---
+++ sensor/ButtonModule/ButtonModule.ino
@@ -0,0 +1,20 @@
+#include <Button.h>
+
+
+Button button3(4);
+
+void setup()
+{
+    Serial.begin(9600);
+    button3.begin();
+    
+}
+
+
+void loop(){
+    while (button3.pressed()) 
+    {
+    Serial.println("HENSHIN!!");
+    }
+
+}
